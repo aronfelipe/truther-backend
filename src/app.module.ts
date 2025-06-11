c24f15044227474727ab142238d381169bed23f8
unknown <felipe.aron@icloud.com>
2025-06-11 16:08:47 -0300

    Feat: new module => cryptocurrencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { UsersModule } from './users/users.module';
+import { CryptocurrenciesModule } from './cryptocurrencies/cryptocurrencies.module';
 
 @Module({
-  imports: [AuthenticationModule, UsersModule],
+  imports: [AuthenticationModule, UsersModule, CryptocurrenciesModule],
   controllers: [AppController],
   providers: [AppService],
 })
